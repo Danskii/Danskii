d752cac10a23ee80fdaa97d725c899ef1f04daef
Daniel Puiatti <dpuiatti@gmail.com>
2018-05-24 19:52:12 -0400

    Update custom-scripts.js

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,1 +1,6 @@
+var button = document.getElementById("chooseYourDestiny");
+var shirtWrapper = document.getElementById("shirtWrapper"); 
 
+button.addEventListener("click", function() {
+shirtWrapper.classList.add("hidden");
+})
