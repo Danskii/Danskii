53c2dc64d3b788637b66c605028f5286d8c34aaa
Daniel Puiatti <dpuiatti@gmail.com>
2018-05-25 09:32:54 -0400

    Delete custom-scripts.js

--- custom-scripts.js
+++
@@ -1,6 +0,0 @@
-var thisButtonTogglesDisplay = document.querySelector("button");
-var displayThisElementWhenClicked = document.querySelector("#shirtAndSignupWrapper");
-
-thisButtonTogglesDisplay.addEventListener("click", function(){
-    displayThisElementWhenClicked.classList.add("hideMe");
-});
