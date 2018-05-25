0b25ee947ad5ced0e0203dd77925a9593fb95c33
Daniel Puiatti <dpuiatti@gmail.com>
2018-05-25 09:15:36 -0400

    Update custom-scripts.js

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,1 +1,7 @@
 
+var thisButtonTogglesDisplay = document.querySelector("button");
+var displayThisElementWhenClicked = document.querySelector("#shirtAndSignupWrapper");
+
+thisButtonTogglesDisplay.addEventListener("click", function(){
+    displayThisElementWhenClicked.classList.add("hideMe");
+});
